ca9403b8177bd39d8c582abe2fa2da9e4d83e495
yagiko0410 <iamnamake@yahoo.co.jp>
2019-05-02 23:57:08 +0900

    ユーザー情報編集機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/user_parms'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
