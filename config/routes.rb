1eb7fe673f98ab78398d21f2d988ac4c1370d648
yagiko0410 <iamnamake@yahoo.co.jp>
2019-05-04 20:37:14 +0900

    ユーザー情報編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#   get 'user/edit'
+
+#   get 'user/update'
+
+#   get 'user/user_params'
+
+#   devise_for :users
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
+
+devise_for :users
+  root 'messages#index'
+  resources :users, only: [:edit, :update]
 end
