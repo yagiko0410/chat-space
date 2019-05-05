dc8acaf53f1737cd29e0f15dc19a8af55089babb
yagiko0410 <iamnamake@yahoo.co.jp>
2019-05-05 22:44:35 +0900

    グループ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ devise_for :users
   resources :users, only: [:edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
   resources :messages, only: [:index, :create]
-  
+  end
 end
